5d2207641dfa81f1384875db12120dfa505fa04a
harshaihub <jharshvardhan9126@gmail.com>
2026-07-29 18:37:45 +0530

    Add files via upload

---
+++ practical_no.1.py
@@ -0,0 +1,22 @@
+# Student Introduction System
+print("=== Student Introduction System ===")
+    
+    # Accept student details
+name = input("Enter student name: ")
+age = input("Enter student age: ")
+course = input("Enter enrolled course: ")
+college = input("Enter college name: ")
+hobbies = input("Enter hobbies (comma separated): ")
+prn=input("Enter the PRN no. of student : ")
+
+    # Display formatted information
+print("\n--- Student Profile ---")
+print(f" Name      : {name}")
+print(f"Age       : {age}")
+print(f"Course    : {course}")
+print(f"college   : {college}")
+print(f" Hobbies   : {hobbies}")
+print("PRN :",prn)
+
+print("------------------------")
+
