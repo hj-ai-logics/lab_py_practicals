80729e726f091eb52fecd1cfe0906edcb1eefd74
Mr. Harshvardhan S. Jadhav <jharshvardhan9126@gmail.com>
2026-08-13 02:30:01 -0700

    Create practical_no.7.py

---
+++ practical_no.7.py
@@ -0,0 +1,23 @@
+# Develop a text analyzer  tool to count words, vowels,spaces,and character in the paragraph 
+#
+
+# Text Analyzer
+
+paragraph = input("Enter a paragraph: ")
+
+words = len(paragraph.split())
+vowels = 0
+spaces = 0
+characters = len(paragraph)
+
+for ch in paragraph:
+    if ch.lower() in "aeiou":
+        vowels += 1
+    if ch == " ":
+        spaces += 1
+
+print("\n--- Text Analysis ---")
+print("Words      :", words)
+print("Vowels     :", vowels)
+print("Spaces     :", spaces)
+print("Characters :", characters)
