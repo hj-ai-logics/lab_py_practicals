b5a51fa70423ead107ca77d0cd71073feb4f1bc7
Mr. Harshvardhan S. Jadhav <jharshvardhan9126@gmail.com>
2026-08-14 13:24:14 +0530

    Add mini project description in practical_no.22.py
    Add project description for a mini project.

---
+++ practical_no.22.py
@@ -0,0 +1,3 @@
+# Mini Project:
+  # Develop a real-world application such as Student Management System / Expense Tracker / Library Management System.
+  # Integrating functions, data structures, lists, dictionaries, file handling, exception handling, modules
