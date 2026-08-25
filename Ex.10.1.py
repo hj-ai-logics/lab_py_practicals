ab1381b1cd6d3fb05b312692083ca4db228757ef
Mr. Harshvardhan S. Jadhav <jharshvardhan9126@gmail.com>
2026-08-25 21:51:22 +0530

    Create Ex.10.1.py

---
+++ Ex.10.1.py
@@ -0,0 +1,6 @@
+prices = [125.50, 890.75, 450.25, 1200.00, 675.40, 950.80]
+
+prices.sort(reverse=True)
+
+print("Prices from highest to lowest:", prices)
+print("Top 3 priciest entries:", prices[:3])
